f80f7611f917702e8daa072788d05c04c65308ee
Abdul Pangandaman <abdul.pangandaman@volenday.com>
2024-11-11 18:17:34 +0800

    animation and layout

--- scenes/level.gd
+++ scenes/level.gd
@@ -4,6 +4,23 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 var laser_scene: PackedScene = load("res://scenes/laser.tscn")
 
+func _ready() -> void:
+	# Stars
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		# position
+		var random_x = rng.randi_range(0, int(size.x))
+		var random_y = rng.randi_range(0, int(size.y))
+		star.position = Vector2(random_x, random_y)
+		
+		# scale
+		var random_scale = rng.randf_range(1, 1.6)
+		star.scale = Vector2(random_scale, random_scale)
+		
+		# animation speed
+		star.speed_scale = rng.randf_range(0.6, 1.4)
+
 func _on_meteor_titmer_timeout() -> void:
 	# 2. Create an instance
 	var meteor = meteor_scene.instantiate()
@@ -15,4 +32,3 @@ func _on_player_laser(player_pos: Vector2) -> void:
 	var laser = laser_scene.instantiate()
 	$Lasers.add_child(laser)
 	laser.position = player_pos
-	print(player_pos)
