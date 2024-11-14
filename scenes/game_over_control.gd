b39dea967fb90fd10542bd22fa346259644ab812
Abdul Pangandaman <abdul.pangandaman@volenday.com>
2024-11-14 08:39:44 +0800

    added countdown in game over screen

--- scenes/game_over_control.gd
+++ scenes/game_over_control.gd
@@ -2,9 +2,22 @@ extends Control
 
 @export var level_scene: PackedScene
 
+var time_elapse: int = 3
+var can_start_again: bool = false
+
 func _ready() -> void:
 	$CenterContainer/VBoxContainer/Label2.text = $CenterContainer/VBoxContainer/Label2.text + str(Global.score)
+	$MarginContainer/Label.visible = false
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed('shoot'):
+	if can_start_again and event.is_action_pressed('shoot'):
 		get_tree().change_scene_to_packed(level_scene)
+	
+func _on_timer_timeout() -> void:
+	if time_elapse > 0:
+		time_elapse -= 1
+		$CenterContainer/VBoxContainer/MarginContainer/Label3.text = str(time_elapse)
+	else:
+		can_start_again = true
+		$CenterContainer/VBoxContainer/MarginContainer/Label3.text = ""
+		$MarginContainer/Label.visible = true
